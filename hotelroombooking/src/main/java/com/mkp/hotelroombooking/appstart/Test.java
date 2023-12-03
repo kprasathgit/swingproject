cfdf41ebc886ef16af66888668c8e2cd449cc2ee
krishna <krishna@LAPTOP-KPRNONBC>
2023-12-03 13:16:39 +0530

    commit 3

--- hotelroombooking/src/main/java/com/mkp/hotelroombooking/appstart/Test.java
+++ hotelroombooking/src/main/java/com/mkp/hotelroombooking/appstart/Test.java
@@ -1,5 +1,13 @@
 package com.mkp.hotelroombooking.appstart;
 
-public class Test {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+public class Test extends JFrame {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	JOptionPane.showMessageDialog(null, "Test");
+	}
+	
 }
